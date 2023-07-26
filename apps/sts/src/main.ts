1d6e0954902052afaf147a9ccbd503c1d5fa2165
Hayden Young <hayden@hbjy.dev>
2023-07-27 00:47:17 +0100

    feat: use logfmt in sts logs

--- apps/sts/src/main.ts
+++ apps/sts/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LogfmtConsoleLogger } from "@hCloud/nest-logfmt";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        logger: new LogfmtConsoleLogger(),
+    });
     await app.listen(process.env['PORT'] ?? '4000');
 }
 
--- libs/nest/logfmt/src/logfmt.ts
+++ libs/nest/logfmt/src/logfmt.ts
@@ -3,17 +3,32 @@ import { stringify } from "logfmt";
 
 export class LogfmtConsoleLogger extends ConsoleLogger {
     protected formatPid(pid: number) {
-        return `${pid}`;
+        return String(pid);
+    }
+
+    protected formatContext(context: string) {
+        return context;
+    }
+
+    protected colorize(message: string, logLevel: LogLevel) {
+        return message;
+    }
+
+    protected getTimestamp() {
+        return new Date().toISOString();
     }
 
     protected formatMessage(logLevel: LogLevel, message: unknown, pidMessage: string, formattedLogLevel: string, contextMessage: string, timestampDiff: string): string {
+        const context = contextMessage.trim().replaceAll('[', '').replaceAll(']', '');
+
         const output = this.stringifyMessage(message, logLevel);
+
         return stringify({
             pid: pidMessage,
             timestamp: this.getTimestamp(),
-            logLevel,
-            context: contextMessage,
+            level: logLevel,
+            context,
             message: output,
-        });
+        }) + '\n';
     }
 }
